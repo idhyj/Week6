d3d60706a2a3f6748b8f5ad2844d0de549216447
idhyj <145718308+idhyj@users.noreply.github.com>
2023-10-17 19:16:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,23 @@
 let shapeType;
 let shapeSize;
 let button1, button2, button3;
-let timer = 0;
-let sizeChanged = false;
-let colorChanged = false;
+let timer = 0; // Timer variable to control the duration for size change
+let sizeChanged = false; // Flag to indicate if the shape size has changed
+let colorChanged = false; // Flag to indicate if the shape color has changed
 let x, y;
-let xSpeed, ySpeed;
+let xSpeed, ySpeed; // Speed at which the shape moves
 let sound1, sound2, sound3;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background('#ff69b4'); // hot pink color
+  background('#ff69b4');
 
+  // Load sound files for the buttons
   sound1 = loadSound('yeah.mp3');
   sound2 = loadSound('Tada.mp3');
   sound3 = loadSound('puuu.mp3');
 
+  // Initialize shape properties and button positions
   shapeType = ROUND;
   shapeSize = random(50, 150);
   x = random(width);
@@ -34,20 +36,24 @@ function setup() {
 }
 
 function draw() {
-  background('#ff69b4'); // hot pink color
+  background('#ff69b4'); 
   drawShape(x, y, shapeSize, shapeType);
-  moveShape();
+  moveShape(); 
   
+  // Reset shape size to 100 if sizeChanged is true and more than 500 milliseconds have passed since the last size change
   if (sizeChanged && millis() - timer > 500) {
     shapeSize = 100; // set original size
-    sizeChanged = false;
+    sizeChanged = false; // Reset sizeChanged flag
   }
 }
 
+// Function to draw the shape based on its type and properties
 function drawShape(x, y, size, type) {
   noStroke();
   rectMode(CENTER);
   ellipseMode(CENTER);
+
+  // Draw shape based on its type (ROUND, SQUARE, or TRIANGLE)
   if (type === ROUND) {
     ellipse(x, y, size, size);
   } else if (type === SQUARE) {
@@ -58,37 +64,43 @@ function drawShape(x, y, size, type) {
   }
 }
 
+// Function to change the shape type randomly and play 'yeah.mp3' sound
 function changeShape() {
-  shapeType = int(random(3));
-  sound1.play();
+  shapeType = int(random(3)); // Randomly choose 0, 1, or 2 representing ROUND, SQUARE, or TRIANGLE
+  sound1.play(); // Play sound
 }
 
+// Function to change the shape size randomly and play 'Tada.mp3' sound
 function changeSize() {
   shapeSize = random(100, width);
-  timer = millis();
-  sizeChanged = true;
-  sound2.play();
+  timer = millis(); // Record the current time in milliseconds
+  sizeChanged = true; // Set sizeChanged flag to true
+  sound2.play(); // Play sound
 }
 
+// Function to change the shape color randomly, set colorChanged flag to true, and play 'puuu.mp3' sound
 function changeColor(){
   fill(random(255), random(255), random(255));
   colorChanged = true;
   sound3.play();
 }
 
+// Function to move the shape based on its current speed
 function moveShape() {
-  x += xSpeed;
-  y += ySpeed;
-  
+  x += xSpeed; // Move the shape horizontally
+  y += ySpeed; // Move the shape vertically
+
+  // If the shape reaches the canvas boundaries, reverse its direction
   if (x > width || x < 0) {
-    xSpeed *= -1;
+    xSpeed *= -1; // Reverse horizontal direction
   }
   
   if (y > height || y < 0) {
-    ySpeed *= -1;
+    ySpeed *= -1; // Reverse vertical direction
   }
 }
 
+//constants representing the types of shapes
 const ROUND = 0;
 const SQUARE = 1;
 const TRIANGLE = 2;
